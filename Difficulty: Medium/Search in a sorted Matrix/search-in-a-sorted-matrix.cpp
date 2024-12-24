65438083bc8b74c5c89b71635dc4da77f3b75435
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2024-12-24 10:19:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int i=0;
+        int j=(mat[0].size()*mat.size())-1;
+        
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            int row = mid/mat[0].size();
+            int col = mid%mat[0].size();
+            
+            if(mat[row][col]==x){
+                return true;
+            }else if(mat[row][col]<x){
+                i=mid+1;
+            }else{
+                j = mid-1;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
