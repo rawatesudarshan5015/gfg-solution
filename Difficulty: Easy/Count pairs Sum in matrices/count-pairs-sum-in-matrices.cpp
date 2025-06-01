d93db05d2ae84ade944b2fd46252be69d6130468
Sudarshan Rawate <103653714+rawatesudarshan5015@users.noreply.github.com>
2025-06-01 09:56:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int x) {
+        // code here
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<mat1.size();i++){
+            for(int j=0;j<mat1[0].size();j++){
+                mp[mat1[i][j]]++;
+            }
+        }
+        int count =0;
+        for(int i=0;i<mat2.size();i++){
+            for(int j=0;j<mat2[0].size();j++){
+                if(mp.find(x-mat2[i][j])!=mp.end()){
+                    count += mp[x - mat2[i][j]];
+
+                }
+            }
+        }
+        return count;
+        
+    }
+};
